c5a0f32b8c10b6be40785872abb37b75646a4d21
Devin Mullen <dmullen1994@gmail.com>
2017-09-14 20:30:22 -0400

    app.js started

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA0Q_j5yNdCDCwWd7ZzUnPHxZzY4fO4y-g",
+  authDomain: "trumpattack-c7cb1.firebaseapp.com",
+  databaseURL: "https://trumpattack-c7cb1.firebaseio.com",
+  projectId: "trumpattack-c7cb1",
+  storageBucket: "",
+  messagingSenderId: "351130597661"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var pickBandAid = true;
+var patchBear = true;
+var myBandAid;
+
+$(document).on("click","#offendedButton", function(event) {
+  pickBandAid = true;
+})
+
+$(document).on("click",".bandAids", function(event) {
+  myBandAid = $(this).attr("src");
+  patchBear = true;
+})
+
+$(document).on("click","#theBear", function(event) {
+  database.ref().child("bandAids").push({
+    xCord: event.pageX,
+    yCord: event.pageY,
+    bandAid: myBandAid, 
+  })
+  console.log(event);
+})
+
+
+
+
